2af6dc84c4a92751a62996a469e06fc2cf932688
Mahtab Alam <alammahtab08@gmail.com>
2018-10-15 23:49:18 +0530

    MongoDB Chef Repo

---
+++ custom-mongodb/metadata.rb
@@ -0,0 +1,7 @@
+name        "custom-mongodb"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
