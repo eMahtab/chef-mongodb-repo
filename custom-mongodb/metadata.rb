bee2716c07f4ef0ddb0efb0c9acfd10f2dbd177a
Mahtab Alam <alammahtab08@gmail.com>
2018-10-15 23:53:04 +0530

    Changed Maintainer

--- custom-mongodb/metadata.rb
+++ custom-mongodb/metadata.rb
@@ -1,6 +1,6 @@
 name        "custom-mongodb"
 description 'Example cookbook for MongoDB Berkshelf based install'
-maintainer  "AWS OpsWorks"
+maintainer  "Mahtab Alam"
 license     "Apache 2.0"
 version     "1.0.0"
 
